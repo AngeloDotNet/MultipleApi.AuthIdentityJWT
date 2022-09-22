fef68fc7fd2d42fe395b0d97f304fdfe941ea92d
Angelo Pirola <angelo9871@gmail.com>
2022-09-22 10:49:55 +0200

    Aggiunti Controllers

---
+++ src/WebApi.Authentication/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using WebApi.Authentication.Services.Application;
+
+namespace WebApi.Authentication.Controllers;
+
+public class AuthController : ControllerBase
+{
+    private readonly IIdentityService identityService;
+
+    public AuthController(IIdentityService identityService)
+    {
+        this.identityService = identityService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginInputModel model)
+    {
+        var response = await identityService.LoginAsync(model);
+
+        if (response != null)
+        {
+            return Ok(response);
+        }
+
+        return BadRequest();
+    }
+
+    [AllowAnonymous]
+    [HttpPost("refresh")]
+    public async Task<IActionResult> RefreshToken(RefreshTokenInpuModel inputModel)
+    {
+        var response = await identityService.RefreshTokenAsync(inputModel);
+
+        if (response != null)
+        {
+            return Ok(response);
+        }
+
+        return BadRequest();
+    }
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterInputModel inputModel)
+    {
+        var response = await identityService.RegisterAsync(inputModel);
+
+        if (response.Succeeded)
+        {
+            return Ok(response);
+        }
+
+        return BadRequest(response);
+    }
+}
---
+++ src/WebApi.Authentication/Controllers/Common/BaseController.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Authentication.Controllers.Common;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class BaseController : ControllerBase
+{
+
+}
