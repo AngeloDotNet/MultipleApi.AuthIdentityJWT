55be3e0fd451195b03aea2214fea0ce9b44d41e8
Angelo Pirola <angelo9871@gmail.com>
2022-09-22 10:50:30 +0200

    Aggiunti ViewModels

---
+++ src/WebApi.Authentication/Models/ViewModels/AuthViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Authentication.Models.ViewModels;
+
+public class AuthViewModel
+{
+    public string AccessToken { get; set; }
+
+    public string RefreshToken { get; set; }
+}
---
+++ src/WebApi.Authentication/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Authentication.Models.ViewModels;
+
+public class RegisterViewModel
+{
+    public bool Succeeded { get; set; }
+
+    public IEnumerable<string> Errors { get; set; }
+}
