a986bc99081a7af9f88ad2b71728111892163845
Angelo Pirola <angelo9871@gmail.com>
2022-09-22 10:51:46 +0200

    Aggiunti Settings

---
+++ src/WebApi.Authentication/Settings/RoleNames.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Authentication.Settings;
+
+public class RoleNames
+{
+    public const string Administrator = nameof(Administrator);
+    public const string PowerUser = nameof(PowerUser);
+    public const string User = nameof(User);
+}
